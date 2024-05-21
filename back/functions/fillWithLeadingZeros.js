f0e7c88b692f90e7da164156d3c300627b58d6ca
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-05-21 09:13:19 +0300

    try bag back

--- back/functions/fillWithLeadingZeros.js
+++
@@ -1,9 +0,0 @@
-function fillWithLeadingZeros(str) {
-  // Convert the string to a number to remove any leading zeros
-  const num = Number(str);
-  // Convert the number back to a string and pad it with leading zeros
-  const paddedStr = num.toString().padStart(4, '0');
-  return paddedStr;
-}
-
-module.exports = fillWithLeadingZeros
