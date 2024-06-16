1a9ed7f5101fafc3002aa06145991f6c5d5ecd79
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-06-16 18:12:34 +0300

    * back: BAG-0001 +env.LOCAL_ENVS;

--- back/core/db_connection.js
+++ back/core/db_connection.js
@@ -1,14 +1,44 @@
 const mysql = require('mysql2/promise');
+const axios = require('axios');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  database: process.env.DB_DATABASE,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+async function createDatabasePool() {
+  let dbConfig;
 
-module.exports = pool;
+  if (process.env.LOCAL_ENVS === 'true') {
+    dbConfig = {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    };
+  } else {
+    try {
+      const response = await axios.post('https://cs99850.tmweb.ru/safe/get', {
+        projectId: "bag@back",
+        state: "RUNTIME"
+      });
+
+      const envData = response.data;
+      dbConfig = {
+        database: envData.DB_DATABASE,
+        user: envData.DB_USERNAME,
+        password: envData.DB_PASSWORD,
+        host: envData.DB_HOST,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+      };
+    } catch (error) {
+      console.error('Error fetching environment data:', error);
+      process.exit(1); // Exit the process if unable to fetch environment data
+    }
+  }
+
+  return mysql.createPool(dbConfig);
+}
+
+module.exports = createDatabasePool();
